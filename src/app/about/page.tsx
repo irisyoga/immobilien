0f58392f92325f5025cd2a9f11d8fd79839eaad3
Iryna Haber <gaber.ira@gmail.com>
2026-01-20 19:35:36 +0100

    immobilien

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <section>
+      <h2>About</h2>
+
+      <Link href='/'>Back to home</Link>
+    </section>
+  );
+}
---
+++ src/app/help/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function FAQ() {
+  return (
+    <div>
+      <h2>FAQ</h2>
+
+      <Link href='/'>Back to home</Link>
+    </div>
+  );
+}
